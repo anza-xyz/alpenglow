5580dc026fc8640528e310c6cc75061c58d21b56
Wen <113942165+wen-coding@users.noreply.github.com>
2025-03-17 12:09:08 -0700

    Fix build

--- runtime/src/genesis_utils.rs
+++ runtime/src/genesis_utils.rs
@@ -1,7 +1,6 @@
 use {
     alpenglow_vote::state::VoteState as AlpenglowVoteState,
     log::*,
-    alpenglow_vote::state::VoteState as AlpenglowVoteState,
     solana_feature_set::{self, FeatureSet, FEATURE_NAMES},
     solana_sdk::{
         account::{Account, AccountSharedData},
