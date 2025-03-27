1719dfefec281c7cf93d51093b8ac5a17886cef5
yihau <yihau.chen@icloud.com>
2025-03-27 12:54:58 +0800

    build program

--- build-alpenglow-vote/build.rs
+++ build-alpenglow-vote/build.rs
@@ -1,4 +1,13 @@
+use std::path::Path;
+
 fn main() {
+    if let Some(toolchain) = std::env::var_os("RUSTUP_TOOLCHAIN") {
+        if toolchain.to_string_lossy().contains("nightly") {
+            println!("cargo:warning=skipping cargo-build-sbf check because nightly rust is used");
+            return;
+        }
+    }
+
     println!("cargo:warning=checking for cargo-build-sbf");
 
     let mut cargo_build_sbf = std::process::Command::new("cargo-build-sbf");
@@ -31,6 +40,54 @@ fn main() {
         let version = String::from_utf8_lossy(&output.stdout);
         println!("cargo:warning=cargo-build-sbf version: {}", version);
     } else {
-        eprintln!("cargo:warning=failed to get cargo-build-sbf version");
+        println!("cargo:warning=failed to get cargo-build-sbf version");
+    }
+
+    let noop_so_path = Path::new("./noop.so");
+    if noop_so_path.exists() {
+        std::fs::remove_file(noop_so_path).expect("failed to remove ./noop.so");
+        println!("cargo:warning=removed existing noop.so");
+    }
+
+    let mut program_path =
+        Path::new("../platform-tools-sdk/cargo-build-sbf/tests/crates/noop/Cargo.toml");
+    if !program_path.exists() {
+        program_path =
+            Path::new("/solana/platform-tools-sdk/cargo-build-sbf/tests/crates/noop/Cargo.toml");
+    }
+    println!(
+        "cargo:warning=using program path: {}",
+        program_path.display()
+    );
+
+    let output = std::process::Command::new(cargo_build_sbf.get_program())
+        .arg("--manifest-path")
+        .arg(program_path)
+        .arg("--sbf-out-dir")
+        .arg(".")
+        .output()
+        .expect("failed to run cargo-build-sbf");
+    if !output.status.success() {
+        println!(
+            "cargo:warning=stderr: {}\nstdout: {}",
+            String::from_utf8_lossy(&output.stderr),
+            String::from_utf8_lossy(&output.stdout)
+        );
+        panic!("failed to run cargo-build-sbf");
+    }
+
+    if noop_so_path.exists() {
+        println!("cargo:warning=./noop.so exists");
+        let output = std::process::Command::new("ls")
+            .arg("-l")
+            .arg(noop_so_path)
+            .output()
+            .expect("failed to run ls");
+        println!(
+            "cargo:warning=ls -l ./noop.so: {}",
+            String::from_utf8_lossy(&output.stdout)
+        );
+    } else {
+        println!("cargo:warning=./noop.so does not exist");
     }
 }
