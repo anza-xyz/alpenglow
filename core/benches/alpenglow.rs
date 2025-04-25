4bed793802243fa0f6afeece0c08720df1271549
Wen <113942165+wen-coding@users.noreply.github.com>
2025-04-25 10:17:55 -0700

    Switch to normal benchmarking.

--- core/benches/alpenglow.rs
+++ core/benches/alpenglow.rs
@@ -1,12 +1,16 @@
+#![allow(clippy::arithmetic_side_effects)]
+#![feature(test)]
+extern crate test;
+
 use {
     alpenglow_vote::vote::Vote,
-    criterion::{black_box, criterion_group, criterion_main, Criterion},
     solana_core::alpenglow_consensus::certificate_pool::CertificatePool,
     solana_runtime::{
         bank::Bank,
         genesis_utils::{create_genesis_config_with_vote_accounts, ValidatorVoteKeypairs},
     },
     solana_sdk::{hash::Hash, signer::Signer, transaction::VersionedTransaction},
+    test::Bencher,
 };
 
 pub const NUM_VALIDATORS: usize = 2000;
@@ -34,7 +38,7 @@ fn add_vote_bench<F>(
                 has_error = true;
             }
         }
-        if pool.get_notarization_cert_size(slot).is_none() && !pool.skip_certified(slot) {
+/*        if pool.get_notarization_cert_size(slot).is_none() && !pool.skip_certified(slot) {
             panic!(
                 "Failed to notarize or skip slot {} {} {:?} {}",
                 slot,
@@ -42,11 +46,12 @@ fn add_vote_bench<F>(
                 pool.get_notarization_cert_size(slot),
                 pool.skip_certified(slot)
             );
-        }
+        }*/
     }
 }
 
-pub fn certificate_pool_add_vote_benchmark(c: &mut Criterion) {
+#[bench]
+fn certificate_pool_add_vote_notarize_benchmark(b: &mut Bencher) {
     let validator_keypairs = (0..NUM_VALIDATORS)
         .map(|_| ValidatorVoteKeypairs::new_rand())
         .collect::<Vec<_>>();
@@ -56,28 +61,33 @@ pub fn certificate_pool_add_vote_benchmark(c: &mut Criterion) {
         vec![100; NUM_VALIDATORS],
     );
     let bank = Bank::new_for_tests(&genesis.genesis_config);
-    let mut pool = CertificatePool::new_from_root_bank(&bank);
-    c.bench_function("add_vote_notarize", |b| {
-        b.iter(|| {
-            add_vote_bench(
-                &validator_keypairs,
-                |slot| Vote::new_notarization_vote(slot, Hash::new_unique(), Hash::new_unique()),
-                black_box(&mut pool),
-            );
-        })
-    });
-
-    let mut pool = CertificatePool::new_from_root_bank(&bank);
-    c.bench_function("add_vote_skip", |b| {
-        b.iter(|| {
-            add_vote_bench(
-                &validator_keypairs,
-                Vote::new_skip_vote,
-                black_box(&mut pool),
-            );
-        })
+    b.iter(|| {
+        let mut pool = CertificatePool::new_from_root_bank(&bank);
+        add_vote_bench(
+            &validator_keypairs,
+            |slot| Vote::new_notarization_vote(slot, Hash::new_unique(), Hash::new_unique()),
+            &mut pool,
+        );
     });
 }
 
-criterion_group!(benches, certificate_pool_add_vote_benchmark);
-criterion_main!(benches);
+#[bench]
+fn certificate_pool_add_vote_skip_benchmark(b: &mut Bencher) {
+    let validator_keypairs = (0..NUM_VALIDATORS)
+    .map(|_| ValidatorVoteKeypairs::new_rand())
+    .collect::<Vec<_>>();
+    let genesis = create_genesis_config_with_vote_accounts(
+    1_000_000_000,
+    &validator_keypairs,
+    vec![100; NUM_VALIDATORS],
+    );
+    let bank = Bank::new_for_tests(&genesis.genesis_config);
+        b.iter(|| {
+            let mut pool = CertificatePool::new_from_root_bank(&bank);
+            add_vote_bench(
+                &validator_keypairs,
+                Vote::new_skip_vote,
+                &mut pool,
+            );
+    });
+}
