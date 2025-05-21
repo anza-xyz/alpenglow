6e50f99a9f1fe21120f65a97ae3a075b22738495
samkim-crypto <skim13@cs.stanford.edu>
2025-05-21 18:21:35 -0400

    implement `AlpenglowVoteTransaction` trait for `BLSVoteMessage`

--- core/src/alpenglow_consensus/transaction.rs
+++ core/src/alpenglow_consensus/transaction.rs
@@ -1,4 +1,11 @@
-use {solana_bls::keypair::Keypair as BLSKeypair, solana_sdk::transaction::VersionedTransaction};
+use {
+    alpenglow_vote::{
+        bls_message::VoteMessage,
+        vote::{NotarizationVote, Vote},
+    },
+    solana_bls::keypair::Keypair as BLSKeypair,
+    solana_sdk::transaction::VersionedTransaction,
+};
 
 pub trait AlpenglowVoteTransaction: Clone + std::fmt::Debug {
     fn new_for_test(bls_keypair: BLSKeypair) -> Self;
@@ -7,5 +14,21 @@ pub trait AlpenglowVoteTransaction: Clone + std::fmt::Debug {
 impl AlpenglowVoteTransaction for VersionedTransaction {
     fn new_for_test(_bls_keypair: BLSKeypair) -> Self {
         Self::default()
+    }
+}
+
+impl AlpenglowVoteTransaction for VoteMessage {
+    fn new_for_test(bls_keypair: BLSKeypair) -> Self {
+        // use notarization vote since this is just for tests
+        let vote = Vote::Notarize(NotarizationVote::default());
+
+        // unwrap since this is just for tests
+        let signature = bls_keypair.sign(&bincode::serialize(&vote).unwrap()).into();
+
+        VoteMessage {
+            vote,
+            signature,
+            rank: 0,
+        }
     }
 }
