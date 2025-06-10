27a304a242687d4e7461058bf06e1b004ea8dfd1
Wen <113942165+wen-coding@users.noreply.github.com>
2025-06-10 14:19:58 -0700

    Add alpenglow port and alpenglow sigverify stage. (#202)

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -1660,6 +1660,7 @@ impl Validator {
                 transactions_quic: node.sockets.tpu_quic,
                 transactions_forwards_quic: node.sockets.tpu_forwards_quic,
                 vote_quic: node.sockets.tpu_vote_quic,
+                alpenglow: node.sockets.alpenglow,
             },
             &rpc_subscriptions,
             transaction_status_sender,
@@ -1800,6 +1801,10 @@ impl Validator {
         info!(
             "local retransmit address: {}",
             node.sockets.retransmit_sockets[0].local_addr().unwrap()
+        );
+        info!(
+            "local alpenglow address: {}",
+            node.sockets.alpenglow.local_addr().unwrap()
         );
     }
 
