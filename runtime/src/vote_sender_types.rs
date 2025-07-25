f7fc4365a35a89c5035c200e7c2e2d94b78b316b
Wen <113942165+wen-coding@users.noreply.github.com>
2025-07-24 22:30:15 -0700

    Remove AlpenglowVoteSender and related functions.

--- runtime/src/vote_sender_types.rs
+++ runtime/src/vote_sender_types.rs
@@ -1,15 +1,11 @@
 use {
-    alpenglow_vote::{bls_message::BLSMessage, vote::Vote as AlpenglowVote},
+    alpenglow_vote::bls_message::BLSMessage,
     crossbeam_channel::{Receiver, Sender},
-    solana_sdk::{pubkey::Pubkey, transaction::VersionedTransaction},
     solana_vote::vote_parser::ParsedVote,
 };
 
 pub type ReplayVoteSender = Sender<ParsedVote>;
 pub type ReplayVoteReceiver = Receiver<ParsedVote>;
-
-pub type AlpenglowVoteSender = Sender<(AlpenglowVote, Pubkey, VersionedTransaction)>;
-pub type AlpenglowVoteReceiver = Receiver<(AlpenglowVote, Pubkey, VersionedTransaction)>;
 
 pub type BLSVerifiedMessageSender = Sender<BLSMessage>;
 pub type BLSVerifiedMessageReceiver = Receiver<BLSMessage>;
