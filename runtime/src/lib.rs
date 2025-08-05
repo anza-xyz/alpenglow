2d5e139726c0130f801d9ab8cdaff6e7b322c8e4
Jon C <me@jonc.dev>
2025-08-05 05:55:15 -0400

    SPL: Use token-2022-interface crate (#7298)
    As outlined in #7256, we need to get Agave off of crates that depend
    back on Agave.
    
    spl-token-2022 is one of those crates
    
    Use spl-token-2022-interface instead. To go with this, we had to also
    upgrade to confidential-transfer-proof-extract v0.4.0, which requires
    modifications to some tests.
    
    Everything should be very straightforward otherwise, and we get to see
    some red lines in the lockfile!

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(feature = "frozen-abi", feature(min_specialization))]
 #![allow(clippy::arithmetic_side_effects)]
+// TODO(ksn): remove this
+#![allow(deprecated)]
 
 mod account_saver;
 pub mod accounts_background_service;
