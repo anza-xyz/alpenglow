3b8717bc92d1e1ec44be764a4a01d531876d4405
Wen <113942165+wen-coding@users.noreply.github.com>
2025-10-16 19:28:22 -0700

    Send Alpenglow Last Voted Slot from BLS Sigverifier to RPC.

---
+++ rpc/src/alpenglow_last_voted.rs
@@ -0,0 +1,54 @@
+// This is a temporay work around to correctly report last votes of peers in Alpenglow
+// before we have the certs in banks.
+use {
+    solana_clock::Slot,
+    solana_pubkey::Pubkey,
+    std::{collections::HashMap, sync::RwLock, time::Instant},
+};
+
+pub struct AlpenglowLastVoted {
+    last_voted_map: RwLock<HashMap<Pubkey, (Slot, Instant)>>,
+}
+
+impl AlpenglowLastVoted {
+    pub fn new() -> Self {
+        Self {
+            last_voted_map: RwLock::new(HashMap::new()),
+        }
+    }
+
+    pub fn update_last_voted(&self, verified_votes_by_pubkey: &HashMap<Pubkey, Vec<Slot>>) {
+        let now = Instant::now();
+        let mut map = self.last_voted_map.write().unwrap();
+        for (pubkey, slots) in verified_votes_by_pubkey {
+            let largest_slot = slots.iter().max().unwrap();
+            map.entry(*pubkey)
+                .and_modify(|entry| *entry = ((*entry).0.max(*largest_slot), now))
+                .or_insert((*largest_slot, now));
+        }
+        map.retain(|_, (_, time)| now.duration_since(*time).as_secs() <= 3600);
+    }
+
+    pub fn get_last_voted(&self, pubkey: &Pubkey) -> Option<Slot> {
+        self.last_voted_map.read().unwrap().get(pubkey).map(|(slot, _)| *slot)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_alpenglow_last_voted() {
+        let alpenglow_last_voted = AlpenglowLastVoted::new();
+        let pubkey1 = Pubkey::new_unique();
+        let pubkey2 = Pubkey::new_unique();
+        alpenglow_last_voted.update_last_voted(&HashMap::from([
+            (pubkey1, vec![1]),
+            (pubkey2, vec![2]),
+        ]));
+        assert_eq!(alpenglow_last_voted.get_last_voted(&pubkey1), Some(1));
+        assert_eq!(alpenglow_last_voted.get_last_voted(&pubkey2), Some(2));
+        assert_eq!(alpenglow_last_voted.get_last_voted(&Pubkey::new_unique()), None);
+    }
+}
