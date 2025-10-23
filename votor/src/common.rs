54bccfdedcf43b5c7d5e6bf0fd698e37b2ec0dbc
akhi singhania <akhi3030@gmail.com>
2025-10-23 10:46:25 +0000

    wip

--- votor/src/common.rs
+++ votor/src/common.rs
@@ -10,27 +10,6 @@ use {
 // Core consensus types and constants
 pub type Stake = u64;
 
-pub const fn conflicting_types(vote_type: VoteType) -> &'static [VoteType] {
-    match vote_type {
-        VoteType::Finalize => &[VoteType::NotarizeFallback, VoteType::Skip],
-        VoteType::Notarize => &[VoteType::Skip, VoteType::NotarizeFallback],
-        VoteType::NotarizeFallback => &[VoteType::Finalize, VoteType::Notarize],
-        VoteType::Skip => &[
-            VoteType::Finalize,
-            VoteType::Notarize,
-            VoteType::SkipFallback,
-        ],
-        VoteType::SkipFallback => &[VoteType::Skip],
-        VoteType::Genesis => &[
-            VoteType::Finalize,
-            VoteType::Notarize,
-            VoteType::NotarizeFallback,
-            VoteType::Skip,
-            VoteType::SkipFallback,
-        ],
-    }
-}
-
 /// Lookup from `CertificateId` to the `VoteType`s that contribute,
 /// as well as the stake fraction required for certificate completion.
 ///
