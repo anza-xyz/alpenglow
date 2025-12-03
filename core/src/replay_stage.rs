9b0a86a218be5ba20a6b1e9d3184fe89edd98047
Ashwin Sekar <ashwin@anza.xyz>
2025-12-03 17:05:25 +0000

    pr feedback: add back debug_assert in replay, link issue

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -922,6 +922,9 @@ impl ReplayStage {
 
                 let forks_root = bank_forks.read().unwrap().root();
                 let start_leader_time = if !migration_status.is_alpenglow_enabled() {
+                    // Will address this in https://github.com/anza-xyz/alpenglow/issues/619
+                    debug_assert!(!votor_event_receiver.is_full());
+
                     // Process cluster-agreed versions of duplicate slots for which we potentially
                     // have the wrong version. Our version was dead or pruned.
                     // Signalled by ancestor_hashes_service.
