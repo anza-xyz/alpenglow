3cd87f4589663132f057a5dbb5382bab62682397
akhi singhania <akhi3030@gmail.com>
2025-12-19 09:30:06 +0000

    introduces consensus reward container

---
+++ votor/src/consensus_rewards.rs
@@ -0,0 +1,275 @@
+use {
+    crossbeam_channel::{select_biased, Receiver, Sender},
+    entry::Entry,
+    solana_clock::Slot,
+    solana_gossip::cluster_info::ClusterInfo,
+    solana_ledger::leader_schedule_cache::LeaderScheduleCache,
+    solana_votor_messages::{
+        consensus_message::VoteMessage,
+        reward_certificate::{NotarRewardCertificate, SkipRewardCertificate},
+        vote::Vote,
+    },
+    std::{
+        collections::BTreeMap,
+        sync::{
+            atomic::{AtomicBool, Ordering},
+            Arc,
+        },
+        thread::{self, Builder, JoinHandle},
+        time::Duration,
+    },
+};
+
+mod entry;
+
+/// Number of slots in the past that the the current leader is responsible for producing the reward certificates.
+const NUM_SLOTS_FOR_REWARD: u64 = 8;
+
+/// Returns [`false`] if the rewards container is not interested in the [`VoteMessage`].
+/// Returns [`true`] if the rewards container might be interested in the [`VoteMessage`].
+pub fn wants_vote(
+    cluster_info: &ClusterInfo,
+    leader_schedule: &LeaderScheduleCache,
+    root_slot: Slot,
+    vote: &VoteMessage,
+) -> bool {
+    match vote.vote {
+        Vote::Notarize(_) | Vote::Skip(_) => (),
+        Vote::Finalize(_)
+        | Vote::NotarizeFallback(_)
+        | Vote::SkipFallback(_)
+        | Vote::Genesis(_) => return false,
+    }
+    let vote_slot = vote.vote.slot();
+    if vote_slot.saturating_add(NUM_SLOTS_FOR_REWARD) < root_slot {
+        return false;
+    }
+    let my_pubkey = cluster_info.id();
+    let Some(leader) =
+        leader_schedule.slot_leader_at(vote_slot.saturating_add(NUM_SLOTS_FOR_REWARD), None)
+    else {
+        return false;
+    };
+    if leader != my_pubkey {
+        return false;
+    }
+    true
+}
+
+/// Container to store state needed to generate reward certificates.
+struct ConsensusRewards {
+    /// Per [`Slot`], stores skip and notar votes.
+    votes: BTreeMap<Slot, Entry>,
+    /// Stores the latest pubkey for the current node.
+    cluster_info: Arc<ClusterInfo>,
+    /// Stores the leader schedules.
+    leader_schedule: Arc<LeaderScheduleCache>,
+    /// Flag to indicate when the channel receiving loop should exit.
+    exit: Arc<AtomicBool>,
+    /// Channel to receive messages to build reward certificates.
+    build_reward_certs_receiver: Receiver<BuildRewardCertsRequest>,
+    /// Channel send the built reward certificates.
+    reward_certs_sender: Sender<BuildRewardCertsResponse>,
+    /// Channel to receive verified votes.
+    votes_receiver: Receiver<AddVoteMessage>,
+}
+
+impl ConsensusRewards {
+    /// Constructs a new instance of [`ConsensusRewards`].
+    fn new(
+        cluster_info: Arc<ClusterInfo>,
+        leader_schedule: Arc<LeaderScheduleCache>,
+        exit: Arc<AtomicBool>,
+        build_reward_certs_receiver: Receiver<BuildRewardCertsRequest>,
+        reward_certs_sender: Sender<BuildRewardCertsResponse>,
+        votes_receiver: Receiver<AddVoteMessage>,
+    ) -> Self {
+        Self {
+            votes: BTreeMap::default(),
+            cluster_info,
+            leader_schedule,
+            exit,
+            build_reward_certs_receiver,
+            reward_certs_sender,
+            votes_receiver,
+        }
+    }
+
+    /// Runs a loop receiving and handling messages over different channels.
+    fn run(&mut self) {
+        while !self.exit.load(Ordering::Relaxed) {
+            // bias messages to build certificates as that is on the critical path
+            select_biased! {
+                recv(self.build_reward_certs_receiver) -> msg => {
+                    match msg {
+                        Ok(msg) => {
+                            let (skip, notar) = self.build_certs(msg.slot);
+                            let resp = BuildRewardCertsResponse {
+                                skip,
+                                notar,
+                            };
+                            if self.reward_certs_sender.send(resp).is_err() {
+                                warn!("cert sender channel is disconnected; exiting.");
+                                break;
+                            }
+                        }
+                        Err(_) => {
+                            warn!("build reward certs channel is disconnected; exiting.");
+                            break;
+                        }
+                    }
+                }
+                recv(self.votes_receiver) -> msg => {
+                    match msg {
+                        Ok(msg) => {
+                            for entry in msg.votes {
+                                self.add_vote(msg.root_slot, entry.max_validators, &entry.vote);
+                            }
+                        }
+                        Err(_) => {
+                            warn!("votes receiver channel is disconnected; exiting.");
+                            break;
+                        }
+                    }
+                }
+                default(Duration::from_secs(1)) => {
+                    continue;
+                }
+            }
+        }
+    }
+
+    /// Returns [`true`] if the rewards container is interested in this vote else [`false`].
+    fn wants_vote(&self, root_slot: Slot, vote: &VoteMessage) -> bool {
+        if !wants_vote(&self.cluster_info, &self.leader_schedule, root_slot, vote) {
+            return false;
+        }
+        let Some(entry) = self.votes.get(&vote.vote.slot()) else {
+            return true;
+        };
+        entry.wants_vote(vote)
+    }
+
+    /// Adds received [`VoteMessage`]s from other validators.
+    fn add_vote(&mut self, root_slot: Slot, max_validators: usize, vote: &VoteMessage) {
+        // drop old state no longer needed
+        self.votes = self.votes.split_off(
+            &(root_slot
+                .saturating_add(NUM_SLOTS_FOR_REWARD)
+                .saturating_add(1)),
+        );
+
+        if !self.wants_vote(root_slot, vote) {
+            return;
+        }
+        match self
+            .votes
+            .entry(vote.vote.slot())
+            .or_insert(Entry::new(max_validators))
+            .add_vote(vote)
+        {
+            Ok(()) => (),
+            Err(e) => {
+                warn!("Adding vote {vote:?} failed with {e}");
+            }
+        }
+    }
+
+    /// Builds reward certificates.
+    fn build_certs(
+        &self,
+        slot: Slot,
+    ) -> (
+        Option<SkipRewardCertificate>,
+        Option<NotarRewardCertificate>,
+    ) {
+        match self.votes.get(&slot) {
+            None => (None, None),
+            Some(entry) => {
+                let skip = entry.build_skip_cert(slot);
+                let notar = entry.build_notar_cert(slot);
+                let skip = match skip {
+                    Ok(s) => s,
+                    Err(e) => {
+                        warn!("Build skip reward cert failed with {e}");
+                        None
+                    }
+                };
+                let notar = match notar {
+                    Ok(n) => n,
+                    Err(e) => {
+                        warn!("Build notar reward cert failed with {e}");
+                        None
+                    }
+                };
+                (skip, notar)
+            }
+        }
+    }
+}
+
+/// Message to add votes to the rewards container.
+pub struct AddVoteMessage {
+    /// The current root slot.
+    pub root_slot: Slot,
+    /// List of [`AddVoteEntry`], one per vote.
+    pub votes: Vec<AddVoteEntry>,
+}
+
+/// Data structure for sending per vote state in the [`AddVoteMessage`].
+pub struct AddVoteEntry {
+    /// Maximum number of validators in the slot that this vote is for.
+    pub max_validators: usize,
+    /// The actual vote.
+    pub vote: VoteMessage,
+}
+
+/// Request to build reward certificates.
+pub struct BuildRewardCertsRequest {
+    /// The slot for which the certs should be built for.
+    pub slot: Slot,
+}
+
+/// Response of building reward certificates.
+pub struct BuildRewardCertsResponse {
+    /// Skip reward certificate.  None if building failed or no skip votes were registered.
+    pub skip: Option<SkipRewardCertificate>,
+    /// Notar reward certificate.  None if building failed or no notar votes were registered.
+    pub notar: Option<NotarRewardCertificate>,
+}
+
+/// Service to run the consensus reward container in a dedicated thread.
+pub struct ConsensusRewardsService {
+    handle: JoinHandle<()>,
+}
+
+impl ConsensusRewardsService {
+    pub fn new(
+        cluster_info: Arc<ClusterInfo>,
+        leader_schedule: Arc<LeaderScheduleCache>,
+        votes_receiver: Receiver<AddVoteMessage>,
+        build_reward_certs_receiver: Receiver<BuildRewardCertsRequest>,
+        reward_certs_sender: Sender<BuildRewardCertsResponse>,
+        exit: Arc<AtomicBool>,
+    ) -> Self {
+        let handle = Builder::new()
+            .name("solConsRew".to_string())
+            .spawn(move || {
+                ConsensusRewards::new(
+                    cluster_info,
+                    leader_schedule,
+                    exit,
+                    build_reward_certs_receiver,
+                    reward_certs_sender,
+                    votes_receiver,
+                )
+                .run();
+            })
+            .unwrap();
+        Self { handle }
+    }
+
+    pub fn join(self) -> thread::Result<()> {
+        self.handle.join()
+    }
+}
---
+++ votor/src/consensus_rewards/entry/partial_cert.rs
@@ -0,0 +1,216 @@
+use {
+    super::{AddVoteError, BuildCertError},
+    bitvec::{order::Lsb0, vec::BitVec},
+    solana_bls_signatures::{
+        Signature as BLSSignature, SignatureCompressed as BLSSignatureCompressed,
+        SignatureProjective,
+    },
+    solana_signer_store::encode_base2,
+    solana_votor_messages::consensus_message::VoteMessage,
+};
+
+/// State for when we have not seen votes from all validators.
+pub(super) struct InProgressState {
+    /// In progress signature aggregate.
+    signature: SignatureProjective,
+    /// bitvec of ranks whose signatures is included in the aggregate above.
+    bitvec: BitVec<u8, Lsb0>,
+    /// number of signatures in the aggregate above.
+    cnt: usize,
+}
+
+/// State for when we have seen votes from all the validators so the cert can be pre-built.
+pub(super) struct DoneState {
+    /// The final aggregate signature.
+    signature: BLSSignatureCompressed,
+    /// Bitmap of rank of validators included in the aggregate above.
+    bitmap: Vec<u8>,
+    /// max number of validators for this slot.
+    max_validators: usize,
+}
+
+/// Struct to hold state for building a single reward cert.
+pub(super) enum PartialCert {
+    /// Variant for when we have not seen votes from all validators.
+    InProgress(InProgressState),
+    /// Variant for when we have seen votes from all the validators.
+    Done(DoneState),
+}
+
+impl PartialCert {
+    /// Returns a new instance of [`PartialCert`].
+    pub(super) fn new(max_validators: usize) -> Self {
+        let state = InProgressState {
+            signature: SignatureProjective::identity(),
+            bitvec: BitVec::repeat(false, max_validators),
+            cnt: 0,
+        };
+        Self::InProgress(state)
+    }
+
+    /// Returns true if the [`PartialCert`] needs the vote else false.
+    pub(super) fn wants_vote(&self, vote: &VoteMessage) -> bool {
+        match self {
+            Self::Done(_) => false,
+            Self::InProgress(state) => match state.bitvec.get(vote.rank as usize) {
+                None => false,
+                Some(ind) => !*ind,
+            },
+        }
+    }
+
+    /// Adds the given [`VoteMessage`] to the aggregate.
+    pub(super) fn add_vote(&mut self, vote: &VoteMessage) -> Result<(), AddVoteError> {
+        match self {
+            Self::Done(_) => Err(AddVoteError::Duplicate),
+            Self::InProgress(state) => {
+                match state.bitvec.get_mut(vote.rank as usize) {
+                    None => return Err(AddVoteError::InvalidRank),
+                    Some(mut ind) => {
+                        if *ind {
+                            return Err(AddVoteError::Duplicate);
+                        }
+                        state
+                            .signature
+                            .aggregate_with(std::iter::once(&vote.signature))?;
+                        *ind = true;
+                    }
+                }
+                state.cnt = state.cnt.saturating_add(1);
+                if state.cnt == state.bitvec.len() {
+                    let signature = BLSSignature::from(state.signature).try_into().unwrap();
+                    *self = Self::Done(DoneState {
+                        signature,
+                        bitmap: encode_base2(&state.bitvec).map_err(AddVoteError::Encode)?,
+                        max_validators: state.bitvec.len(),
+                    });
+                }
+                Ok(())
+            }
+        }
+    }
+
+    /// Builds a signature and associated bitmap from the collected votes.
+    ///
+    /// Returns Ok(None) if no votes were collected.
+    pub(super) fn build_sig_bitmap(
+        &self,
+    ) -> Result<Option<(BLSSignatureCompressed, Vec<u8>)>, BuildCertError> {
+        match self {
+            Self::Done(state) => Ok(Some((state.signature, state.bitmap.clone()))),
+            Self::InProgress(state) => {
+                if state.cnt == 0 {
+                    return Ok(None);
+                }
+                let mut bitvec = state.bitvec.clone();
+                let new_len = bitvec.last_one().map_or(0, |i| i.saturating_add(1));
+                bitvec.resize(new_len, false);
+                let bitmap = encode_base2(&bitvec).map_err(BuildCertError::Encode)?;
+                let signature = BLSSignature::from(state.signature).try_into().unwrap();
+                Ok(Some((signature, bitmap)))
+            }
+        }
+    }
+
+    /// Returns how many votes have been seen.
+    pub(super) fn votes_seen(&self) -> usize {
+        match self {
+            Self::InProgress(state) => state.cnt,
+            Self::Done(state) => state.max_validators,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        solana_bls_signatures::Keypair as BLSKeypair,
+        solana_signer_store::{decode, Decoded},
+        solana_votor_messages::vote::Vote,
+    };
+
+    fn validate_bitmap(bitmap: &[u8], num_set: usize, max_len: usize) {
+        let bitvec = decode(bitmap, max_len).unwrap();
+        match bitvec {
+            Decoded::Base2(bitvec) => assert_eq!(bitvec.count_ones(), num_set),
+            Decoded::Base3(_, _) => panic!("unexpected variant"),
+        }
+    }
+
+    fn new_vote(vote: Vote, rank: usize) -> VoteMessage {
+        let serialized = bincode::serialize(&vote).unwrap();
+        let keypair = BLSKeypair::new();
+        let signature = keypair.sign(&serialized).into();
+        VoteMessage {
+            vote,
+            signature,
+            rank: rank.try_into().unwrap(),
+        }
+    }
+
+    #[test]
+    fn validate_votes_seen() {
+        let max_validators = 2;
+        let skip = Vote::new_skip_vote(7);
+        let mut partial_cert = PartialCert::new(max_validators);
+        for rank in 0..max_validators {
+            let vote = new_vote(skip, rank);
+            partial_cert.add_vote(&vote).unwrap();
+            assert_eq!(partial_cert.votes_seen(), rank + 1);
+        }
+    }
+
+    #[test]
+    fn validate_build_sig_bitmap() {
+        let max_validators = 2;
+        let mut partial_cert = PartialCert::new(max_validators);
+        assert!(matches!(partial_cert.build_sig_bitmap(), Ok(None)));
+        let skip = Vote::new_skip_vote(7);
+        for rank in 0..max_validators {
+            let vote = new_vote(skip, rank);
+            partial_cert.add_vote(&vote).unwrap();
+            let (_signature, bitmap) = partial_cert.build_sig_bitmap().unwrap().unwrap();
+            validate_bitmap(&bitmap, rank + 1, max_validators);
+        }
+    }
+
+    #[test]
+    fn validate_add_vote() {
+        let mut partial_cert = PartialCert::new(2);
+        let skip = Vote::new_skip_vote(7);
+        let vote = new_vote(skip, 2);
+        assert!(matches!(
+            partial_cert.add_vote(&vote),
+            Err(AddVoteError::InvalidRank)
+        ));
+        let vote = new_vote(skip, 0);
+        partial_cert.add_vote(&vote).unwrap();
+        assert!(matches!(
+            partial_cert.add_vote(&vote),
+            Err(AddVoteError::Duplicate)
+        ));
+        let vote = new_vote(skip, 1);
+        partial_cert.add_vote(&vote).unwrap();
+        let vote = new_vote(skip, 0);
+        assert!(matches!(
+            partial_cert.add_vote(&vote),
+            Err(AddVoteError::Duplicate)
+        ));
+    }
+
+    #[test]
+    fn validate_wants_vote() {
+        let mut partial_cert = PartialCert::new(2);
+        let skip = Vote::new_skip_vote(7);
+        let vote = new_vote(skip, 2);
+        assert!(!partial_cert.wants_vote(&vote));
+        let vote = new_vote(skip, 0);
+        assert!(partial_cert.wants_vote(&vote));
+        partial_cert.add_vote(&vote).unwrap();
+        assert!(!partial_cert.wants_vote(&vote));
+        let vote = new_vote(skip, 1);
+        partial_cert.add_vote(&vote).unwrap();
+        assert!(!partial_cert.wants_vote(&vote));
+    }
+}
