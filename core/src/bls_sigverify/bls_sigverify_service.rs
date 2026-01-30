0f7ab07f01a0fd3f4187eb826f2a532866bc5cdd
samkim-crypto <skim13@cs.stanford.edu>
2026-01-30 19:23:25 +0900

    refactor `BLSSigVerifyService::run` into modular steps

--- core/src/bls_sigverify/bls_sigverify_service.rs
+++ core/src/bls_sigverify/bls_sigverify_service.rs
@@ -75,21 +75,63 @@ impl BLSSigVerifyService {
         let mut deduper = Deduper::<2, [u8]>::new(&mut rng, DEDUPER_NUM_BITS);
 
         loop {
+            // Packet Receive Step
+            let (mut batches, num_packets, recv_duration) =
+                match Self::receive_packets(&packet_receiver) {
+                    ReceiveAction::Process(b, n, d) => (b, n, d),
+                    ReceiveAction::Continue => continue,
+                    ReceiveAction::Break => break,
+                };
+
+            let batches_len = batches.len();
+            debug!(
+                "@{:?} bls_verifier: verifying: {}",
+                timing::timestamp(),
+                num_packets,
+            );
+
+            // Dedupe Packet Step
             if deduper.maybe_reset(&mut rng, DEDUPER_FALSE_POSITIVE_RATE, MAX_DEDUPER_AGE) {
                 stats.num_deduper_saturations += 1;
             }
-            if let Err(e) = Self::verifier(&deduper, &packet_receiver, &mut verifier, &mut stats) {
-                match e {
-                    BLSSigVerifyError::Streamer(StreamerError::RecvTimeout(
-                        RecvTimeoutError::Disconnected,
-                    )) => break,
-                    BLSSigVerifyError::Streamer(StreamerError::RecvTimeout(
-                        RecvTimeoutError::Timeout,
-                    )) => (),
+            let (discard_or_dedup_fail, dedup_time_us) =
+                Self::dedup_packets(&deduper, &mut batches);
+
+            // BLS Signature Verification (and Send) Step
+            //
+            // TODO(sam): Currently, verification result is sent inside the verification function.
+            //            Consider refactoring out the send step out of the signature verification
+            //            step.
+            let verify_time_us = Self::verify_packets(&mut verifier, batches);
+
+            match verify_time_us {
+                Ok(verify_time_us) => {
+                    debug!(
+                        "@{:?} verifier: done. batches: {} total verify time: {:?} verified: {} \
+                         v/s {}",
+                        timing::timestamp(),
+                        batches_len,
+                        verify_time_us / 1000,
+                        num_packets,
+                        (num_packets as f32 / (verify_time_us as f32 / 1_000_000.0))
+                    );
+
+                    Self::update_stats(
+                        &mut stats,
+                        num_packets,
+                        batches_len,
+                        recv_duration,
+                        dedup_time_us,
+                        verify_time_us,
+                        discard_or_dedup_fail,
+                    );
+                }
+                Err(e) => match e {
                     BLSSigVerifyError::Send(_) | BLSSigVerifyError::TrySend(_) => break,
                     _ => error!("{e:?}"),
-                }
+                },
             }
+
             if last_print.elapsed().as_secs() > 2 {
                 stats.maybe_report();
                 stats = BLSPacketStats::default();
@@ -98,63 +140,80 @@ impl BLSSigVerifyService {
         }
     }
 
-    fn verifier<const K: usize>(
+    fn receive_packets(packet_receiver: &Receiver<PacketBatch>) -> ReceiveAction {
+        match streamer::recv_packet_batches(packet_receiver) {
+            Ok((batches, num_packets, recv_duration)) => {
+                ReceiveAction::Process(batches, num_packets, recv_duration)
+            }
+            Err(e) => match e {
+                StreamerError::RecvTimeout(RecvTimeoutError::Disconnected) => ReceiveAction::Break,
+                StreamerError::RecvTimeout(RecvTimeoutError::Timeout) => ReceiveAction::Continue,
+                _ => {
+                    error!("{e:?}");
+                    ReceiveAction::Continue
+                }
+            },
+        }
+    }
+
+    fn dedup_packets<const K: usize>(
         deduper: &Deduper<K, [u8]>,
-        recvr: &Receiver<PacketBatch>,
-        verifier: &mut BLSSigVerifier,
-        stats: &mut BLSPacketStats,
-    ) -> Result<(), BLSSigVerifyError> {
-        let (mut batches, num_packets, recv_duration) = streamer::recv_packet_batches(recvr)?;
-
-        let batches_len = batches.len();
-        debug!(
-            "@{:?} bls_verifier: verifying: {}",
-            timing::timestamp(),
-            num_packets,
-        );
-
+        batches: &mut [PacketBatch],
+    ) -> (usize, u64) {
         let mut dedup_time = Measure::start("bls_sigverify_dedup_time");
         let discard_or_dedup_fail =
-            deduper::dedup_packets_and_count_discards(deduper, &mut batches) as usize;
+            deduper::dedup_packets_and_count_discards(deduper, batches) as usize;
         dedup_time.stop();
+        (discard_or_dedup_fail, dedup_time.as_us())
+    }
 
+    fn verify_packets(
+        verifier: &mut BLSSigVerifier,
+        batches: Vec<PacketBatch>,
+    ) -> Result<u64, BLSSigVerifyError> {
         let mut verify_time = Measure::start("sigverify_batch_time");
         verifier.verify_and_send_batches(batches)?;
         verify_time.stop();
+        Ok(verify_time.as_us())
+    }
 
-        debug!(
-            "@{:?} verifier: done. batches: {} total verify time: {:?} verified: {} v/s {}",
-            timing::timestamp(),
-            batches_len,
-            verify_time.as_ms(),
-            num_packets,
-            (num_packets as f32 / verify_time.as_s())
-        );
-
+    fn update_stats(
+        stats: &mut BLSPacketStats,
+        num_packets: usize,
+        batches_len: usize,
+        recv_duration: Duration,
+        dedup_time_us: u64,
+        verify_time_us: u64,
+        discard_or_dedup_fail: usize,
+    ) {
         stats
             .recv_batches_us_hist
             .increment(recv_duration.as_micros() as u64)
             .unwrap();
         stats
             .verify_batches_pp_us_hist
-            .increment(verify_time.as_us() / (num_packets as u64))
+            .increment(verify_time_us / (num_packets as u64))
             .unwrap();
         stats
             .dedup_packets_pp_us_hist
-            .increment(dedup_time.as_us() / (num_packets as u64))
+            .increment(dedup_time_us / (num_packets as u64))
             .unwrap();
         stats.batches_hist.increment(batches_len as u64).unwrap();
         stats.packets_hist.increment(num_packets as u64).unwrap();
         stats.total_batches += batches_len;
         stats.total_packets += num_packets;
         stats.total_dedup += discard_or_dedup_fail;
-        stats.total_dedup_time_us += dedup_time.as_us() as usize;
-        stats.total_verify_time_us += verify_time.as_us() as usize;
-
-        Ok(())
+        stats.total_dedup_time_us += dedup_time_us as usize;
+        stats.total_verify_time_us += verify_time_us as usize;
     }
 
     pub fn join(self) -> thread::Result<()> {
         self.thread_hdl.join()
     }
+}
+
+enum ReceiveAction {
+    Process(Vec<PacketBatch>, usize, Duration),
+    Continue,
+    Break,
 }
