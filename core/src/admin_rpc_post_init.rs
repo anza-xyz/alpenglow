lijunwangs/solana

Send SetIdentity from admin_rpc_service. (#363)

core/src/admin_rpc_post_init.rs
@@ -8,6 +8,7 @@ use {
     solana_quic_definitions::NotifyKeyUpdate,
     solana_runtime::bank_forks::BankForks,
     solana_streamer::atomic_udp_socket::AtomicUdpSocket,
+    solana_votor::event::VotorEventSender,
     std::{
         collections::{HashMap, HashSet},
         net::UdpSocket,
@@ -80,4 +81,5 @@ pub struct AdminRpcRequestMetadataPostInit {
     pub outstanding_repair_requests: Arc<RwLock<OutstandingRequests<ShredRepairType>>>,
     pub cluster_slots: Arc<ClusterSlots>,
     pub gossip_socket: Option<AtomicUdpSocket>,
+    pub votor_event_sender: VotorEventSender,
 }

anza-xyz/alpenglow

core/src/admin_rpc_post_init.rs
@@ -4,6 +4,7 @@ use {
         cluster_slots_service::cluster_slots::ClusterSlots,
         repair::{outstanding_requests::OutstandingRequests, serve_repair::ShredRepairType},
     },
+    agave_votor::event::VotorEventSender,
     solana_gossip::{cluster_info::ClusterInfo, node::NodeMultihoming},
     solana_pubkey::Pubkey,
     solana_runtime::{bank_forks::BankForks, snapshot_controller::SnapshotController},
@@ -87,4 +88,5 @@ pub struct AdminRpcRequestMetadataPostInit {
     pub node: Option<Arc<NodeMultihoming>>,
     pub banking_control_sender: mpsc::Sender<BankingControlMsg>,
     pub snapshot_controller: Arc<SnapshotController>,
+    pub votor_event_sender: VotorEventSender,
 }